210a9cb92a73bd2be7b2991716e355f935847e94
Alexandre Koroneos <akoroneos@outlook.de>
2024-11-19 18:06:31 +0100

    Finish assignment 2 (shell)

--- uebung2/shell.c
+++ uebung2/shell.c
@@ -1,4 +1,6 @@
-#include <stdio.h> 
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,24 +14,18 @@
 #include "vec.h"
 
 #define PATH_MAX 4096
+#define ARGS_MAX 2
 
-// TODO: Funktionen auslagern
+pid_t monitored_process = 0;
 
-int cd_sh() {
-	return 0;
-}
-
-int wait_sh() {
-	return 0;
-}
-
-int exit_sh() {
-	// TODO: Graceful exit: Alle Ressourcen freigeben
-	exit(0);
+void sig_handler(int signo) {
+	if (monitored_process > 0) {
+		kill(monitored_process, SIGINT);
+	} // else ignore
 }
 
 int main(void) {
-	signal(SIGINT, SIG_IGN);
+	signal(SIGINT, sig_handler);
 
 	setvbuf(stdout, NULL, _IOLBF, BUFSIZ);
 	setvbuf(stdin, NULL, _IOLBF, BUFSIZ);
@@ -46,45 +42,77 @@ int main(void) {
 		char input_buf[BUFSIZ];
 		read(STDIN_FILENO, input_buf, BUFSIZ); // wait until line break
 
-		// Process input (max 1 argument)
-		char* command = strtok(input_buf, " \n");
-		char* arg = strtok(NULL, " \n");
-		// printf("command: %s\narg: %s\n", command, arg);
-		// TODO: clear after exec
-		// TODO: accept multiple arguments
+		char* args[10];
+		args[0] = strtok(input_buf, " \n");
 
-		if (strcmp(command, "cd") == 0) {
+		char* arg;
+		int i = 1;
+		while ((arg = strtok(NULL, " \n")) != NULL) {
+			args[i++] = arg;
+		}
+		args[i] = NULL;
+		int argc = i - 1;
+
+		// Process input
+		if (strcmp(args[0], "cd") == 0) {
 			char path[PATH_MAX];
 
-			if (arg[0] == '/') { // absolute path
-				snprintf(path, sizeof(path), "%s", arg);
+			if (args[0][0] == '/') { // absolute path
+				snprintf(path, sizeof(path), "%s", args[0]);
 			} else { // relative path
-				snprintf(path, sizeof(path), "%s/%s", cwd, arg);
+				snprintf(path, sizeof(path), "%s/%s", cwd, args[0]);
 			}
 
 			if (chdir(path) != 0) {
 				perror("chdir failed");
 			}
-		} else if (strcmp(command, "wait") == 0) { // TODO: sobald Programmausführung und unäres & abschlossen (ermöglicht Test)
-			pid_t pid = atoi(arg);
-			int status;
-			if (waitpid(pid, &status, 0)) {
-				perror("wait");
+
+		} else if (strcmp(args[0], "wait") == 0) {
+			pid_t monitored_pids[argc];
+			int handled[argc];
+			for (size_t i = 1; i <= argc; i++) {
+				monitored_pids[i] = atoi(args[i]);
+				handled[i] = 0;
 			}
-		} else if (strcmp(command, "exit") == 0) {
-			exit_sh();
-		} else { // Execute files
-			// pid_t child = fork();
-			// if (child == 0) {
-			// 	int status;
-			// 	waitpid(child, &status, 0);
-			// 	break;
-			// } else {
-			// 	execv(command, arg);
-			// 	exit(-1);
-			// }
-			// TODO: asynchrones Ausführen (tracing '&') -> waitpid(fork())
+
+			int remaining = argc;
+			while (remaining > 0) { // FIXME: Infinite loop after some calls to wait
+				int status = 0;
+				pid_t finished_process = wait(&status);
+
+				for (size_t j = 1; j <= argc; j++) {
+					if (!handled[j] && monitored_pids[j] == finished_process) {
+						printf("[%d] TERMINATED\n[%d] EXIT CODE: %d\n", finished_process, finished_process, WEXITSTATUS(status));
+						handled[j] = 1;
+						remaining--;
+						break;
+					}
+				}
+			}
+
+			// TODO: Ctrl+C
+		} else if (strcmp(args[0], "exit") == 0) {
+			exit(0);
+		} else { // Execute file at path arg[0] with args
+			int is_async = (strcmp(args[argc], "&") == 0);
+
+			monitored_process = fork();
+			if (monitored_process == 0) {
+				execv(args[0], args);
+				perror("execv failed");
+				exit(-1);
+			} else if (is_async) {
+				// FIXME: Output appearing after next shell prompt
+				printf("[%d]\n", monitored_process);
+			} else {
+				int status;
+				waitpid(monitored_process, &status, 0);
+			}
+
+			// TODO: Pipes
 		}
+
+		monitored_process = 0;
 	}
 	return 0;
 }
