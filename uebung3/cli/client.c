117e1f06d234a32d419deab810ade350db9ef325
Alexandre Koroneos <akoroneos@outlook.de>
2024-12-02 19:16:33 +0100

    Switch to clang-format

--- uebung3/cli/client.c
+++ uebung3/cli/client.c
@@ -1,83 +1,83 @@
-#include "protocol.h"
-
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "protocol.h"
 
 #define PORT 9000
 #define HOST "127.0.0.1"
 
 int intializeClientSocket() {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in srv_addr = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = inet_addr(HOST),
-		.sin_port = htons(PORT)
-	};
+    struct sockaddr_in srv_addr = {
+        .sin_family = AF_INET, .sin_addr.s_addr = inet_addr(HOST), .sin_port = htons(PORT)};
 
-	connect(sockfd, (struct sockaddr*)&srv_addr, sizeof(struct sockaddr_in));
+    connect(sockfd, (struct sockaddr*)&srv_addr, sizeof(struct sockaddr_in));
 
-	return sockfd;
+    return sockfd;
 }
 
 int main() {
-	setvbuf(stdin, NULL, _IOLBF, BUFSIZ);
+    setvbuf(stdin, NULL, _IOLBF, BUFSIZ);
 
-	int srv = intializeClientSocket();
+    int srv = intializeClientSocket();
 
-	while (1) {
-		char msg_type;
+    while (1) {
+        char msg_type;
 
-		if (read(srv, &msg_type, 1) == 0) {
-			printf("Server has closed the connection.\n");
-			break;
-		}
+        if (read(srv, &msg_type, 1) == 0) {
+            printf("Server has closed the connection.\n");
+            break;
+        }
 
-		char recvBuf[BUFSIZ];
-		if (msg_type == MSG_OUTPUT) {
-			read(srv, recvBuf, sizeof(recvBuf));
-			printf("%s", recvBuf);
-			fflush(stdout);
-		} else if (msg_type == MSG_PROMPT) {
-			read(srv, recvBuf, sizeof(recvBuf));
-			printf("%s", recvBuf); // print prompt
-			fflush(stdout);
+        char recvBuf[BUFSIZ];
+        if (msg_type == MSG_OUTPUT) {
+            read(srv, recvBuf, sizeof(recvBuf));
+            printf("%s", recvBuf);
+            fflush(stdout);
+        } else if (msg_type == MSG_PROMPT) {
+            read(srv, recvBuf, sizeof(recvBuf));
+            printf("%s", recvBuf);  // print prompt
+            fflush(stdout);
 
-			// read user input
-			char input_buf[BUFSIZ];
-			read(STDIN_FILENO, input_buf, sizeof(input_buf));
+            // read user input
+            char input_buf[BUFSIZ];
+            read(STDIN_FILENO, input_buf, sizeof(input_buf));
 
-			char* command = strtok(input_buf, " \n");
+            char* command = strtok(input_buf, " \n");
 
-			if (!strcmp(command, "put")) {
-				// send command
-				// wait for response?
-				// open file
-				// send file size
-				// send file contents
-			} else if (!strcmp(command, "get")) {
-				// send command
-				// wait for response?
-				// read file size
-				// open file (according to size?)
-				// read file contents into file
-			} else {
-				input_buf[strlen(command)] = ' '; // remove NULL-character set by strok
-				write(srv, input_buf, strlen(input_buf));
-			}
-		}
-	}
+            if (!strcmp(command, "put")) {
+                input_buf[strlen(command)] = ' ';  // remove NULL-character set by strok
+                write(srv, input_buf, strlen(input_buf));
 
-	// Close connection
-	shutdown(srv, SHUT_RDWR);
-	close(srv);
-	printf("Client Exit\n");
+                // wait for response?
+                // open file
+                // send file size
+                // send file contents
+            } else if (!strcmp(command, "get")) {
+                input_buf[strlen(command)] = ' ';  // remove NULL-character set by strok
+                write(srv, input_buf, strlen(input_buf));
 
-	return 0;
+                // wait for response?
+                // read file size
+                // open file (according to size?)
+                // read file contents into file
+            } else {
+                input_buf[strlen(command)] = ' ';  // remove NULL-character set by strok
+                write(srv, input_buf, strlen(input_buf));
+            }
+        }
+    }
+
+    // Close connection
+    shutdown(srv, SHUT_RDWR);
+    close(srv);
+    printf("Client Exit\n");
+
+    return 0;
 }
--- uebung3/srv/server.c
+++ uebung3/srv/server.c
@@ -1,16 +1,15 @@
-#include "protocol.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <arpa/inet.h>
 #include <errno.h>
-
-#include <sys/socket.h>
-#include <sys/wait.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "protocol.h"
 
 #define PORT 9000
 
@@ -18,48 +17,48 @@
 #define ARGS_MAX 8
 
 int initializeServerSocket() {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	int sockopt = 1;
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&sockopt, sizeof(sockopt)); // enable immediate reuse of this port after closing the socket
-	setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char*)&sockopt, sizeof(sockopt)); // disable delayed tcp segments
+    int sockopt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&sockopt,
+               sizeof(sockopt));  // enable immediate reuse of this port after closing the socket
+    setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char*)&sockopt,
+               sizeof(sockopt));  // disable delayed tcp segments
 
-	struct sockaddr_in srv_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(PORT),
-		.sin_addr.s_addr = INADDR_ANY
-	};
+    struct sockaddr_in srv_addr = {
+        .sin_family = AF_INET, .sin_port = htons(PORT), .sin_addr.s_addr = INADDR_ANY};
 
-	if (bind(sockfd, (struct sockaddr*)&srv_addr, sizeof(struct sockaddr_in)) < 0) {
-		printf("Couldn't bind to address");
-		if (errno == EADDRINUSE)
-			printf(": Adress already in use");
-		fflush(stdout);
+    if (bind(sockfd, (struct sockaddr*)&srv_addr, sizeof(struct sockaddr_in)) < 0) {
+        printf("Couldn't bind to address");
+        if (errno == EADDRINUSE) printf(": Adress already in use");
+        fflush(stdout);
 
-		exit(-1);
-	}
+        exit(-1);
+    }
 
-	listen(sockfd, 0);
+    listen(sockfd, 0);
 
-	return sockfd;
+    return sockfd;
 }
 
 void writePrompt(int fd) {
-	char* cwd = getcwd(NULL, 0);
+    char* cwd = getcwd(NULL, 0);
 
-	char prompt[strlen(cwd) + 4]; // + msg_type + '>' + ' ' + string terminator
-	snprintf(prompt, sizeof(prompt), "%c%s> ", MSG_PROMPT, cwd); // prefix prompt with message type indicator
+    char prompt[strlen(cwd) + 4];  // + msg_type + '>' + ' ' + string terminator
+    snprintf(prompt, sizeof(prompt), "%c%s> ", MSG_PROMPT,
+             cwd);  // prefix prompt with message type indicator
 
-	write(fd, prompt, strlen(prompt) + 1); // +1 to full null-terminated string
+    write(fd, prompt, strlen(prompt) + 1);  // +1 to full null-terminated string
 
-	free(cwd);
+    free(cwd);
 }
 
 void writeOutput(int fd, char* msg) {
-	char prefixed_msg[strlen(msg) + 2]; // + msg_type + string terminator
-	snprintf(prefixed_msg, sizeof(prefixed_msg), "%c%s", MSG_OUTPUT, msg); // prefix msg with message type indicator
+    char prefixed_msg[strlen(msg) + 2];  // + msg_type + string terminator
+    snprintf(prefixed_msg, sizeof(prefixed_msg), "%c%s", MSG_OUTPUT,
+             msg);  // prefix msg with message type indicator
 
-	write(fd, prefixed_msg, sizeof(prefixed_msg) + 1);
+    write(fd, prefixed_msg, sizeof(prefixed_msg) + 1);
 }
 
 /**
@@ -67,82 +66,82 @@ void writeOutput(int fd, char* msg) {
  *
  * @param fd   The file descriptor to read the command from.
  * @param buf  A buffer to store the command read from the file descriptor.
- * @param args An array to store the parsed arguments. args[0] holds the command itself and the last value is NULL (see argv[] for exec() in POSIX).
+ * @param args An array to store the parsed arguments. args[0] holds the command itself and the last
+ * value is NULL (see argv[] for exec() in POSIX).
  * @return     The number of arguments parsed.
  */
 int readCommand(int fd, char* buf, size_t buf_len, char* args[], size_t args_len) {
-	read(fd, buf, buf_len);
+    read(fd, buf, buf_len);
 
-	args[0] = strtok(buf, " \n");
+    args[0] = strtok(buf, " \n");
 
-	char* arg;
-	int i = 1;
-	while ((arg = strtok(NULL, " \n")) != NULL) {
-		args[i++] = arg;
-	}
-	args[i] = NULL;
+    char* arg;
+    int i = 1;
+    while ((arg = strtok(NULL, " \n")) != NULL) {
+        args[i++] = arg;
+    }
+    args[i] = NULL;
 
-	int argc = i - 1;
-	return argc;
+    int argc = i - 1;
+    return argc;
 }
 
 int main() {
-	int sockfd = initializeServerSocket();
+    int sockfd = initializeServerSocket();
 
-	struct sockaddr_in cli_addr;
-	socklen_t addr_len = sizeof(struct sockaddr_in);
-	int cli = accept(sockfd, (struct sockaddr*)&cli_addr, &addr_len);
+    struct sockaddr_in cli_addr;
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    int cli = accept(sockfd, (struct sockaddr*)&cli_addr, &addr_len);
 
-	char msg[] = "Connection established!\n";
-	writeOutput(cli, msg);
+    char msg[] = "Connection established!\n";
+    writeOutput(cli, msg);
 
-	// Shell
-	while (1) {
-		writePrompt(cli);
+    // Shell
+    while (1) {
+        writePrompt(cli);
 
-		char command_buf[BUFSIZ];
-		char* args[ARGS_MAX];
-		readCommand(cli, command_buf, sizeof(command_buf), args, sizeof(args));
-		char* command = args[0];
+        char command_buf[BUFSIZ];
+        char* args[ARGS_MAX];
+        readCommand(cli, command_buf, sizeof(command_buf), args, sizeof(args));
+        char* command = args[0];
 
-		if (!strcmp(command, "exit")) {
-			shutdown(cli, SHUT_RDWR);
-			close(cli);
-			exit(0);
-		} else if (!strcmp(command, "cd")) {
-			char path[PATH_MAX];
+        if (!strcmp(command, "exit")) {
+            shutdown(cli, SHUT_RDWR);
+            close(cli);
+            exit(0);
+        } else if (!strcmp(command, "cd")) {
+            char path[PATH_MAX];
 
-			if (args[1][0] == '/') { // absolute path
-				snprintf(path, sizeof(path), "%s", args[1]);
-			} else { // relative path
-				char* cwd = getcwd(NULL, 0);
-				snprintf(path, sizeof(path), "%s/%s", cwd, args[1]);
-				free(cwd);
-			}
+            if (args[1][0] == '/') {  // absolute path
+                snprintf(path, sizeof(path), "%s", args[1]);
+            } else {  // relative path
+                char* cwd = getcwd(NULL, 0);
+                snprintf(path, sizeof(path), "%s/%s", cwd, args[1]);
+                free(cwd);
+            }
 
-			if (chdir(path) != 0)
-				perror("chdir failed");
-		} else if (!strcmp(command, "get")) { // TODO:
+            if (chdir(path) != 0) perror("chdir failed");
+        } else if (!strcmp(command, "get")) {  // TODO:
 
-		} else if (!strcmp(command, "put")) { // TODO:
-			// open file (if nonexistent)
-			// confirm ready to receive
-			// read from socket
-		} else { // execute file
-			int monitored_process = fork();
-			if (monitored_process == 0) {
-				execvp(command, args);
-				exit(-1);
-			} else {
-				int status;
-				waitpid(monitored_process, &status, 0);
-			}
-		}
-	}
+        } else if (!strcmp(command, "put")) {  // TODO:
+                                               // open file (if nonexistent)
+                                               // confirm ready to receive
+                                               // read from socket
+        } else {                               // execute file
+            int monitored_process = fork();
+            if (monitored_process == 0) {
+                execvp(command, args);
+                exit(-1);
+            } else {
+                int status;
+                waitpid(monitored_process, &status, 0);
+            }
+        }
+    }
 
-	// Close connection
-	shutdown(cli, SHUT_RDWR);
-	close(cli);
+    // Close connection
+    shutdown(cli, SHUT_RDWR);
+    close(cli);
 
-	return 0;
+    return 0;
 }
